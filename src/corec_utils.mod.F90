c0f491f096485fb7b13efe6803daf887dbd4d06f
Tobias Kloeffel <tobias.kloeffel@fau.de>
2019-03-13 11:55:20 +0100

    cp_grp_aware openmp optimization timer

--- src/corec_utils.mod.F90
+++ src/corec_utils.mod.F90
@@ -2,6 +2,8 @@ MODULE corec_utils
   USE cppt,                            ONLY: indz,&
                                              inyh,&
                                              nzh
+  USE cp_grp_utils,                    ONLY: cp_grp_get_sizes,&
+                                             cp_grp_redist_array
   USE fft_maxfft,                      ONLY: maxfftn
   USE fftmain_utils,                   ONLY: invfftn
   USE fftnew_utils,                    ONLY: setfftn
@@ -40,30 +42,37 @@ CONTAINS
     ! ==       out total charge densities in real space               ==
     ! == VTMP: out core charges in G space                            ==
     ! ==--------------------------------------------------------------==
-    REAL(real_8)                             :: rhoe(fpar%nnr1,clsd%nlsd)
-    COMPLEX(real_8)                          :: vtmp(ncpw%nhg), v(maxfftn)
+    REAL(real_8),INTENT(INOUT)               :: rhoe(fpar%nnr1,clsd%nlsd)
+    COMPLEX(real_8),INTENT(OUT)              :: vtmp(ncpw%nhg), v(maxfftn)
 
     COMPLEX(real_8)                          :: ei123
     INTEGER                                  :: ia, ig, ir, is, isa, isa0, &
-                                                isub
+                                                isub, ig_start, ig_end
     REAL(real_8)                             :: vvre
+    CHARACTER(*), PARAMETER                  :: procedureN = 'corec'
 
-    CALL tiset('     COREC',isub)
+    CALL tiset(procedureN,isub)
+
     CALL setfftn(0)
     ! SUM CORE CHARGES IN G-SPACE
-    CALL zeroing(vtmp)!,nhg)
+    CALL cp_grp_get_sizes(first_nhg=ig_start,last_nhg=ig_end)
+    !$omp parallel private(ig,isa0,is,ia,isa,ei123)
+    !$omp do
+    DO ig=ig_start,ig_end
+       vtmp(ig)=CMPLX(0.0_real_8,0.0_real_8)
+    END DO
+    !$omp end do nowait
     IF (cntl%bigmem) THEN
        isa0=0
        DO is=1,ions1%nsp
           IF (corel%tnlcc(is)) THEN
              DO ia=1,ions0%na(is)
                 isa=isa0+ia
-#ifdef __SR8000
-                !poption parallel, tlocal(IG)
-#endif 
-                DO ig=1,ncpw%nhg
+                !$omp do
+                DO ig=ig_start,ig_end
                    vtmp(ig)=vtmp(ig)+rhoc(ig,is)*eigrb(ig,isa)
                 ENDDO
+                !$omp end do nowait
              ENDDO
           ENDIF
           isa0=isa0+ions0%na(is)
@@ -74,24 +83,23 @@ CONTAINS
           IF (corel%tnlcc(is)) THEN
              DO ia=1,ions0%na(is)
                 isa=isa0+ia
-#ifdef __SR8000
-                !poption parallel, tlocal(IG,EI123)
-#endif 
-                DO ig=1,ncpw%nhg
+                !$omp do
+                DO ig=ig_start,ig_end
                    ei123=ei1(isa,inyh(1,ig))*ei2(isa,inyh(2,ig))*&
                         ei3(isa,inyh(3,ig))
                    vtmp(ig)=vtmp(ig)+rhoc(ig,is)*ei123
                 ENDDO
+                !$omp end do nowait
              ENDDO
           ENDIF
           isa0=isa0+ions0%na(is)
        ENDDO
     ENDIF
+    !$omp end parallel
+    IF(parai%cp_nogrp.GT.1)CALL cp_grp_redist_array(vtmp,ncpw%nhg)
     ! FFT TO R-SPACE
     CALL zeroing(v)!,maxfft)
-#ifdef __SR8000
-    !poption parallel, tlocal(IG)
-#endif 
+    !$omp parallel do private(ig)
     DO ig=1,ncpw%nhg
        v(nzh(ig))=vtmp(ig)
        v(indz(ig))=CONJG(vtmp(ig))
@@ -114,7 +122,7 @@ CONTAINS
           rhoe(ir,1)=rhoe(ir,1)+REAL(v(ir))
        ENDDO
     ENDIF
-    CALL tihalt('     COREC',isub)
+    CALL tihalt(procedureN,isub)
     ! ==--------------------------------------------------------------==
     RETURN
   END SUBROUTINE corec
