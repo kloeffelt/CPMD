fa5fbf2b206bc5c280b97635436902178c55c499
Tobias Kloeffel <tobias.kloeffel@fau.de>
2019-03-19 17:12:48 +0100

    Bugfix: openmp reduction of fion leads to strange errors

--- src/potfor_utils.mod.F90
+++ src/potfor_utils.mod.F90
@@ -1,7 +1,3 @@
-#if defined(__SR11000)
-!option OPT(O(ss))
-#endif
-
 MODULE potfor_utils
   USE cppt,                            ONLY: gk,&
                                              inyh,&
@@ -55,13 +51,9 @@ CONTAINS
     omtp=2._real_8*parm%omega*parm%tpiba
     ig1=1
     IF (geq0) ig1=2
-#if defined (__VECTOR)
     IF (cntl%bigmem) THEN
        !$omp parallel do private(ISA,IA,IS,IG,EI123,RP,RHET,RHOG,RHETS,RHOGS, &
        !$omp  GX,GY,GZ,VCGS)
-#ifdef _vpp_
-       !OCL NOALIAS
-#endif
        DO isa=1,ions1%nat
           ia=iatpt(1,isa)
           is=iatpt(2,isa)
@@ -85,9 +77,6 @@ CONTAINS
     ELSE
        !$omp parallel do private(ISA,IA,IS,IG,EI123,RP,RHET,RHOG,RHETS,RHOGS, &
        !$omp  GX,GY,GZ,VCGS)
-#ifdef _vpp_
-       !OCL NOALIAS
-#endif
        DO isa=1,ions1%nat
           ia=iatpt(1,isa)
           is=iatpt(2,isa)
@@ -110,91 +99,6 @@ CONTAINS
           ENDDO
        ENDDO
     ENDIF
-#else                
-    ! mb   For scalar version & pseudo-vector machines
-    IF (cntl%bigmem) THEN
-#ifdef __SR8000
-       !poption parallel
-       !poption tlocal(RP,RHET,RHOG,RHETS,RHOGS,GX,GY,GZ,VCGS)
-       !poption tlocal(ISA,TXX,TYY,TZZ,EI123)
-       !poption psum(FION)
-#else
-#ifdef __HPC
-       ! no-OMP - segmentation fault if OMP are used here
-#else
-       !$omp parallel do private(IG,IS,IA,ISA) &
-       !$omp  private(RP,RHET,RHOG,RHETS,RHOGS,GX,GY,GZ,VCGS) &
-       !$omp  private(TXX,TYY,TZZ,EI123) &
-       !$omp  reduction(+:FION)
-#endif
-#endif
-       DO ig=ig1,ncpw%nhg
-          rp=eirop(ig)
-          rhet=v(nzh(ig))
-          rhog=rhet+rp
-          rhets=CONJG(rhet)
-          rhogs=CONJG(rhog)
-          gx=CMPLX(0._real_8,gk(1,ig),kind=real_8)
-          gy=CMPLX(0._real_8,gk(2,ig),kind=real_8)
-          gz=CMPLX(0._real_8,gk(3,ig),kind=real_8)
-          vcgs=scg(ig)*rhogs
-          isa=0
-          DO is=1,ions1%nsp
-             txx=(rhops(is,ig)*vcgs+vps(is,ig)*rhets)*gx
-             tyy=(rhops(is,ig)*vcgs+vps(is,ig)*rhets)*gy
-             tzz=(rhops(is,ig)*vcgs+vps(is,ig)*rhets)*gz
-             DO ia=1,ions0%na(is)
-                isa=isa+1
-                ei123=eigrb(ig,isa)
-                fion(1,ia,is)=fion(1,ia,is)+REAL(ei123*txx)*omtp
-                fion(2,ia,is)=fion(2,ia,is)+REAL(ei123*tyy)*omtp
-                fion(3,ia,is)=fion(3,ia,is)+REAL(ei123*tzz)*omtp
-             ENDDO
-          ENDDO
-       ENDDO
-    ELSE
-#ifdef __SR8000
-       !poption parallel
-       !poption tlocal(RP,RHET,RHOG,RHETS,RHOGS,GX,GY,GZ,VCGS)
-       !poption tlocal(ISA,TXX,TYY,TZZ,EI123)
-       !poption psum(FION)
-#else
-#ifdef __HPC
-       ! no-OMP - segmentation fault if OMP are used here
-#else
-       !$omp parallel do private(IG,IS,IA,ISA) &
-       !$omp  private(RP,RHET,RHOG,RHETS,RHOGS,GX,GY,GZ,VCGS) &
-       !$omp  private(TXX,TYY,TZZ,EI123) &
-       !$omp  reduction(+:FION)
-#endif
-#endif
-       DO ig=ig1,ncpw%nhg
-          rp=eirop(ig)
-          rhet=v(nzh(ig))
-          rhog=rhet+rp
-          rhets=CONJG(rhet)
-          rhogs=CONJG(rhog)
-          gx=CMPLX(0._real_8,gk(1,ig),kind=real_8)
-          gy=CMPLX(0._real_8,gk(2,ig),kind=real_8)
-          gz=CMPLX(0._real_8,gk(3,ig),kind=real_8)
-          vcgs=scg(ig)*rhogs
-          isa=0
-          DO is=1,ions1%nsp
-             txx=(rhops(is,ig)*vcgs+vps(is,ig)*rhets)*gx
-             tyy=(rhops(is,ig)*vcgs+vps(is,ig)*rhets)*gy
-             tzz=(rhops(is,ig)*vcgs+vps(is,ig)*rhets)*gz
-             DO ia=1,ions0%na(is)
-                isa=isa+1
-                ei123=ei1(isa,inyh(1,ig))*ei2(isa,inyh(2,ig))*&
-                     ei3(isa,inyh(3,ig))
-                fion(1,ia,is)=fion(1,ia,is)+REAL(ei123*txx)*omtp
-                fion(2,ia,is)=fion(2,ia,is)+REAL(ei123*tyy)*omtp
-                fion(3,ia,is)=fion(3,ia,is)+REAL(ei123*tzz)*omtp
-             ENDDO
-          ENDDO
-       ENDDO
-    ENDIF
-#endif
     CALL tihalt(procedureN,isub)
     ! ==--------------------------------------------------------------==
     RETURN
@@ -219,7 +123,6 @@ CONTAINS
     omtp=2._real_8*parm%omega*parm%tpiba
     ig1=1
     IF (geq0) ig1=2
-#if defined (__VECTOR) && (! (__NEC))
     isa=0
     DO is=1,ions1%nsp
        DO ia=1,ions0%na(is)
@@ -260,32 +163,6 @@ CONTAINS
           ENDIF
        ENDDO
     ENDDO
-#else
-    DO ig=ig1,ncpw%nhg
-       rg=CONJG(rhog(ig))
-       gx=CMPLX(0._real_8,gk(1,ig),kind=real_8)
-       gy=CMPLX(0._real_8,gk(2,ig),kind=real_8)
-       gz=CMPLX(0._real_8,gk(3,ig),kind=real_8)
-       isa=0
-       DO is=1,ions1%nsp
-          txx=vps(is,ig)*rg*gx
-          tyy=vps(is,ig)*rg*gy
-          tzz=vps(is,ig)*rg*gz
-          DO ia=1,ions0%na(is)
-             isa=isa+1
-             IF (cntl%bigmem) THEN
-                ei123=eigrb(ig,isa)
-             ELSE
-                ei123=ei1(isa,inyh(1,ig))*ei2(isa,inyh(2,ig))*&
-                     ei3(isa,inyh(3,ig))
-             ENDIF
-             fion(1,ia,is)=fion(1,ia,is)+REAL(ei123*txx)*omtp
-             fion(2,ia,is)=fion(2,ia,is)+REAL(ei123*tyy)*omtp
-             fion(3,ia,is)=fion(3,ia,is)+REAL(ei123*tzz)*omtp
-          ENDDO
-       ENDDO
-    ENDDO
-#endif
     CALL tihalt(procedureN,isub)
     ! ==--------------------------------------------------------------==
     RETURN
