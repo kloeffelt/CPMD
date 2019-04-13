710211aeb58fb29b240f03c1b29d5294a641b1f5
Tobias Kloeffel <tobias.kloeffel@fau.de>
2019-04-13 16:54:35 +0200

    cp_grp aware only cp_grp local ngws get updated on request

--- src/velupa_utils.mod.F90
+++ src/velupa_utils.mod.F90
@@ -1,4 +1,5 @@
 MODULE velupa_utils
+  USE cp_grp_utils,                    ONLY: cp_grp_get_sizes
   USE harm,                            ONLY: dtan2c,&
                                              dtan2w,&
                                              freq,&
@@ -22,54 +23,75 @@ MODULE velupa_utils
 CONTAINS
 
   ! ==================================================================
-  SUBROUTINE velupa(c0,cm,c2,nstate,nnlst)
+  SUBROUTINE velupa(c0,cm,c2,nstate,nnlst,use_cp_grps)
     ! ==--------------------------------------------------------------==
-    COMPLEX(real_8)                          :: c0(nkpt%ngwk,*), &
-                                                cm(nkpt%ngwk,*), &
+    COMPLEX(real_8),INTENT(IN)               :: c0(nkpt%ngwk,*), &
                                                 c2(nkpt%ngwk,*)
-    INTEGER                                  :: nstate, nnlst
+    COMPLEX(real_8),INTENT(INOUT)            :: cm(nkpt%ngwk,*)
+    INTEGER,INTENT(IN)                       :: nstate, nnlst
+    LOGICAL,INTENT(IN),OPTIONAL              :: use_cp_grps
 
-    INTEGER                                  :: i, ig, isub
+    LOGICAL                                  :: cp_active
+    INTEGER                                  :: i, ig, isub, &
+                                                ibeg_c0, iend_c0, ngwk_local
     REAL(real_8)                             :: dtx, hfo, hgi
 
-    CALL tiset('    VELUPA',isub)
+    CHARACTER(*), PARAMETER                  :: procedureN = 'velupa'
+
+    CALL tiset(procedureN,isub)
+    IF(PRESENT(use_cp_grps))THEN
+       cp_active=use_cp_grps
+    ELSE
+       cp_active=.FALSE.
+    END IF
+    IF(cp_active)THEN
+       CALL cp_grp_get_sizes(ngwk_l=ngwk_local,firstk_g=ibeg_c0,lastk_g=iend_c0)
+    ELSE
+       ngwk_local=nkpt%ngwk
+       ibeg_c0=1
+       iend_c0=nkpt%ngwk
+    END IF
     ! WAVEFUNCTION VELOCITY UPDATE
     IF (cntl%tharm) THEN
-       !$omp parallel do private(I,IG,HFO,HGI)
+       !$omp parallel do private(i,ig,hfo,hgi)
        DO i=1,nstate
-          DO ig=1,ncpw%ngw
+          DO ig=ibeg_c0,iend_c0
              IF (cntl%tmass) THEN
                 hfo=-xmu(ig)*freq(ig)**2
              ELSE
                 hfo=-cntr%emass*freq(ig)**2
-             ENDIF
+             END IF
              IF (nnlst.EQ.1) THEN
                 hgi=dtan2w(ig)
                 cm(ig,i)=cm(ig,i)+hgi*(c2(ig,i)-hfo*c0(ig,i))
              ELSE
                 hgi=dtan2c(ig)
                 cm(ig,i)=cm(ig,i)+hgi*c2(ig,i)
-             ENDIF
-          ENDDO
-       ENDDO
+             END IF
+          END DO
+       END DO
     ELSE
        IF (cntl%tmass) THEN
-          !$omp parallel do private(I,IG,HGI)
+          !$omp parallel do private(i,ig,hgi)
           DO i=1,nstate
-             DO ig=1,ncpw%ngw
+             DO ig=ibeg_c0,iend_c0
                 hgi=REAL(nnlst,kind=real_8)*0.5_real_8*dt_elec/xmu(ig)
                 cm(ig,i)=cm(ig,i)+hgi*c2(ig,i)
-             ENDDO
-          ENDDO
+             END DO
+          END DO
        ELSE
           dtx=REAL(nnlst,kind=real_8)*dtb2me
-          CALL daxpy(2*nstate*nkpt%ngwk,dtx,c2(1,1),1,cm(1,1),1)
-       ENDIF
-    ENDIF
-    CALL tihalt('    VELUPA',isub)
+          !$omp parallel do private(i,ig)
+          DO i=1,nstate
+             DO ig=ibeg_c0,iend_c0
+                cm(ig,i)=cm(ig,i)+dtx*c2(ig,i)
+             END DO
+          END DO
+       END IF
+    END IF
+    CALL tihalt(procedureN,isub)
     ! ==--------------------------------------------------------------==
     RETURN
   END SUBROUTINE velupa
   ! ==================================================================
-
 END MODULE velupa_utils
